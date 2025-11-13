062661fc605467784ec89fe6c0a0d64bbec4505c
Harsh Srivastava <meharshsri11@gmail.com>
2025-11-13 21:54:32 +0530

    Implemented Canny Edge Detection and JNI call handler in native C++.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,29 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/opencv.hpp>
+#include <android/log.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_myapplication_MainActivity_stringFromJNI(
+#define TAG "V_NATIVE"
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_flamassessment_ImageProcessor_Companion_processFrameNative(
         JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        jobject /* this */,
+        jlong frameBufferAddress,
+        jint width,
+        jint height) {
+
+    if (frameBufferAddress == 0) { return; }
+
+    cv::Mat& mat = *(cv::Mat*)frameBufferAddress;
+
+    // --- Core Processing Logic: Canny Edge Detection ---
+    if (mat.channels() > 1) {
+        cv::cvtColor(mat, mat, cv::COLOR_RGBA2GRAY);
+    }
+    cv::GaussianBlur(mat, mat, cv::Size(5, 5), 0, 0);
+    cv::Canny(mat, mat, 100, 200);
+
+    __android_log_print(ANDROID_LOG_DEBUG, TAG, "Frame processed successfully in C++.");
 }
